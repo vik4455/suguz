e5b3382be595c7700765061cc3b2c916ea3cc4f6
Vikrom Sompong <vikrom@Vikroms-MacBook-Pro.local>
2017-11-17 16:20:02 +0700

    Suguz Test2

--- bot.php
+++ bot.php
@@ -4,7 +4,6 @@ require_once('./vendor/autoload.php');
 use \LINE\LINEBot\HTTPClient\CurlHTTPClient;
 use \LINE\LINEBot;
 use \LINE\LINEBot\MessageBuilder\TextMessageBuilder;
-use \LINE\LINEBot\MessageBuilder\LocationMessageBuilder;
 
 $channel_token = '9Isri8d/LYfcip6hCv4kmAUHR6ST8tTwUYC69hCFEb0wG7TNHEhqqVSrW+KV9rhM5c6xseoifi5zV53qv9L2IBEY8w9Fsn4yv6pLGLsvEIdfGOJrmG2WYB6B7c2X8/McMCSul3HXWnWTqVbK02fWzQdB04t89/1O/w1cDnyilFU=';
 $channel_secret = 'ee36357fc71a88bf26ba4bb51ed4870d';
@@ -18,20 +17,20 @@ if (!is_null($events['events'])) {
 	// Loop through each event
 	foreach ($events['events'] as $event) {
         $replyToken = $event['replyToken']; 
-		$httpClient=new CurlHTTPClient($channel_token); 
-        $bot=new LINEBot($httpClient, array('channelSecret'=> $channel_secret));
-        $response = $bot->getProfile('<userId>');
-        if ($response->isSucceeded()) {
-            $profile = $response->getJSONDecodedBody();
-            $disp =  $profile['displayName'];
-            $pic = $profile['pictureUrl'];
-            $textMessageBuilder=new TextMessageBuilder($disp);
-        }else{
-            $textMessageBuilder=new TextMessageBuilder('ไม่ได้');
+		switch($event['message']['type']) {
+            case 'image':
+                $messageID = $event['message']['id']; 
+                $respMessage='Hello, your image ID is '.$messageID;
+                break; 
+            default:
+                $respMessage='Please send image only'; 
+                break;
         }
         
-        
-        $response = $bot->replyMessage($replyToken, $textMessageBuilder);
+        $httpClient = new CurlHTTPClient($channel_token);
+        $bot=new LINEBot($httpClient, array('channelSecret'=> $channel_secret));
+        $textMessageBuilder=newTextMessageBuilder($respMessage);
+        $response=$bot->replyMessage($replyToken, $textMessageBuilder);
     }
 }
 
