d72c0f4426830e95cf05321909720853b42682f8
Vikrom Sompong <vikrom@Vikroms-MacBook-Pro.local>
2017-11-26 16:13:16 +0700

    Suguz Test4

--- bot.php
+++ bot.php
@@ -7,8 +7,8 @@ use \LINE\LINEBot\MessageBuilder\TextMessageBuilder;
 use \LINE\LINEBot\MessageBuilder\ImageMessageBuilder;
 use \LINE\LINEBot\MessageBuilder\StickerMessageBuilder;
 
-$channel_token = '9Isri8d/LYfcip6hCv4kmAUHR6ST8tTwUYC69hCFEb0wG7TNHEhqqVSrW+KV9rhM5c6xseoifi5zV53qv9L2IBEY8w9Fsn4yv6pLGLsvEIdfGOJrmG2WYB6B7c2X8/McMCSul3HXWnWTqVbK02fWzQdB04t89/1O/w1cDnyilFU=';
-$channel_secret = 'ee36357fc71a88bf26ba4bb51ed4870d';
+$channel_token = '9wekUc+mHLQj9GC8NIUlNqxftM0V+06o7scvwRA6qiNAr4tjnPeWGzTi8ht0FcOz5c6xseoifi5zV53qv9L2IBEY8w9Fsn4yv6pLGLsvEIddjdNLVdz61lOrpfFYAnG/l42YQ89HY2H2bxSPm6UGGAdB04t89/1O/w1cDnyilFU=';
+$channel_secret = 'a482c124ff8e612e52bdd084d1acaa4c';
 
 //Get message from Line API
 $content = file_get_contents('php://input');
