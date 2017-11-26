ae03718a15fb2930334cab1bed181f34eb9ed8e0
Vikrom Sompong <vikrom@Vikroms-MacBook-Pro.local>
2017-11-26 18:30:45 +0700

    Suguz Poll

--- bot.php
+++ bot.php
@@ -25,55 +25,7 @@ if (!is_null($events['events'])) {
             if($event['message']['text']=="คำถาม"){
                 $respMessage = "วันนี้ท๊อฟฟี่จะแพ้อีกรึไม่ กด1 แพ้\n กด2 ไม่แพ้\n";    
             }else if($event['message']['text']=="1" || $event['message']['text']=="2"){
-                try{
-                $host = 'ec2-54-235-65-224.compute-1.amazonaws.com';
-                $dbname = 'd57b0s2qa541bq'; 
-                $user = 'gfqphhprpuzrre';
-                $pass = 'e1c9b3a5cf6a2d33f100944a04ac4b99b53ce0036341b51a0a9988a6e2d527a2';
-                $connection=new PDO("pgsql:host=$host;dbname=$dbname", $user, $pass);
-                
-                $sql=sprintf("SELECT*FROM poll WHERE user_id='%s' ", $event['source']['userId']);
-                $result = $connection->query($sql);
-                error_log($sql);
-                
-                if($result==false || $result->rowCount()<=0){
-                        switch(['message']['text']) {
-                        case '1':
-                            // Insert
-                            $params = array('userID'=> $event['source']['userId'], 'answer'=> '1',);
-                            $statement=$connection->prepare('INSERT INTO poll (user_id,answer)VALUES(:userID, :answer)');
-                            $statement->execute($params);
-                            // Query
-                            $sql=sprintf("SELECT * FROM poll WHERE answer='1' AND user_id='%s'",$event['source']['userId']);
-                            $result = $connection->query($sql);
-                            $amount = 1;
-                            if($result){
-                                $amount = $result->rowCount(); 
-                            }
-                            $respMessage='จํานวนคนตอบว่า แพ้ ='.$amount.' olo';
-                            break;
-                        case '2':
-                            // Insert
-                            $params = array('userID'=> $event['source']['userId'], 'answer'=> '2',);
-                            $statement=$connection->prepare('INSERT INTO poll (user_id,answer)VALUES(:userID, :answer)');
-                            $statement->execute($params);
-                            // Query
-                            $sql=sprintf("SELECT * FROM poll WHERE answer='2' AND user_id='%s'",$event['source']['userId']);
-                            $result = $connection->query($sql);
-                            $amount = 1;
-                            if($result){
-                                $amount = $result->rowCount(); 
-                            }
-                            $respMessage='จํานวนคนตอบว่า ไม่แพ้ ='.$amount.' อิอิ';
-                            break;                    
-                        }    
-                }else{
-                    $respMessage = 'คุณได้ตอบโพลล์นี้แล้ว ไอ้สัส';
-                }
-                   
-            }catch(Exception $e){ 
-                error_log($e->getMessage());
-            }    
+                $respMessage = "XX";    
         }
             
     }
